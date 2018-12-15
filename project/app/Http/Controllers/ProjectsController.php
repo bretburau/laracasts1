b53f4171b448f6034b5ebfaa2a96d1001e55295c
Bret <Burhooo@gmail.com>
2018-12-15 06:54:09 -0600

    laracast ep 11 done

---
+++ project/app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    public function index() {
+        $projects = Project::all();
+        // return $projects;
+        return view('projects.index', compact('projects'));
+    }
+
+    public function create()
+    {
+        return view('projects.create');
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function store()
+    {
+        
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+        
+    }
+
+    public function store()
+    {
+        $project = new Project();
+
+        $project->title = request('title');
+        $project->description = request('description');
+
+        $project->save();
+
+        return redirect('/projects');
+        
+    }
+}
