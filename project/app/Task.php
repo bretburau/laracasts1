f2f7a43c6dfbb259ef28abba40f8f872d7709cce
Bret <Burhooo@gmail.com>
2018-12-21 05:36:25 -0600

    better encapsulation done

--- project/app/Task.php
+++ project/app/Task.php
@@ -7,8 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $guarded = [];
+
+    public function complete($completed = true)
+    {
+        $this->update(compact('completed'));
+    }
+
+    public function incomplete()
+    {
+        $this->complete(false);
+    }
+
     public function project()
     {
         return $this->belongsTo(Project::class);
     }
 }
+//
